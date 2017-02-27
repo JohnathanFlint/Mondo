5e358e1bd7e96d9adb772d7c69f0087d766d71c2
JohnathanFlint <nicholas.white142@gmail.com>
2017-02-27 09:48:31 -0700

    Created the BiDirectionalNode.hpp file.
    Added getters, setters, and constructors.

---
+++ src/Model/BiDirectionalNode.hpp
@@ -0,0 +1,35 @@
+/*
+ * BiDirectionalNode.hpp
+ *
+ *  Created on: Feb 27, 2017
+ *      Author: nwhi5696
+ */
+
+#ifndef MODEL_BIDIRECTIONALNODE_HPP_
+#define MODEL_BIDIRECTIONALNODE_HPP_
+template <class Type>
+class BiDirectionalNode : public Node<Type>
+{
+private:
+	Type Data;
+	BiDirectionalNode<Type> * nextPointer;;
+	BiDirectionalNode<Type> * previousPointer;
+
+public:
+	BiDirectionalNode();
+	BiDirectionalNode(Type data)
+	BiDirectionalNode(Type data, BiDirectionaNode<Type> * previous, BiDirectionalNode<Type> * next);
+
+	Type getNodeData();
+	BiDirectionalNode<Type> * getNextPointer();
+	BiDirectionalNode<Type> * getPreviousPointer();
+
+	void setNodeData(Type data);
+	void setNexPointer(BiDirectionalnode<Type> * next);
+	void setNextPointer(BiDirectionalNode<Type> * previous);
+};
+
+
+
+
+#endif /* MODEL_BIDIRECTIONALNODE_HPP_ */
