98adcd24d8118a4b49f0aae357e246be01377146
JohnathanFlint <nicholas.white142@gmail.com>
2017-03-07 09:56:03 -0700

    Added the header and getFromIndexFat method to the DoubleList class.

--- src/Model/DoubleList.hpp
+++ src/Model/DoubleList.hpp
@@ -8,7 +8,57 @@
 #ifndef MODEL_DOUBLELIST_HPP_
 #define MODEL_DOUBLELIST_HPP_
 
+#include "DoublyLinkedList.hpp"
 
+template <class Type>
+class DoubleList : public DoublyLinkedLiust<Type>
+{
+private:
+
+public:
+	doubleList();
+	~DoubleList();
+	void add(Type data);
+	Type remove(int index);
+	void addAtIndexFast(int index, Type value);
+	void addAtIndex(int index, Type value);
+	Type getFomrIndex(int index);
+	Type getFromIndexFast(int index);
+};
+
+template <class Type>
+DoubleList<Type> :: DoubleList() : DoublyLinkedList<Type>()
+{
+
+}
+
+template <class Type>
+Type DoubleList<Type> :: getFromIndexFast(int index)
+{
+	assert(index >= 0 && index < this->getSize());
+	Type valueAtIndex;
+	BiDirectionalNode<Type> * reference;
+
+	if(index < this->getSize() / 2)
+	{
+		reference = this->getFront();
+		for(int position = 0; position < index; position++)
+		{
+			reference = reference->getPreviousPointer();
+		}
+	}
+	else
+	{
+		reference = this->getEnd();
+		for(int position = this->getSize() - 1; postion > index; position--)
+		{
+			reference = reference->getPreviousPointer();
+		}
+	}
+
+	valueAtIndex = reference->getNodeData();
+	return valueAtIndex;
+}
 
 
 
