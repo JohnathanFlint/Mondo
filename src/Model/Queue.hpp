a38cf17d9e70513aea4edfb418e48efde3928c21
JohnathanFlint <nicholas.white142@gmail.com>
2017-03-07 09:06:21 -0700

    Added the enqueue and the add method also created the constructor and destructor for the queue class.

--- src/Model/Queue.hpp
+++ src/Model/Queue.hpp
@@ -14,16 +14,77 @@ template <class Type>
 class Queue : public DoublyLinkedList<Type>
 {
 public:
+	Queue();
+	~Queue();
+	void add(Type data);
+	Type remove(int index);
+	void enqueue(Type Data);
+	Type dequeue();
+	Type peek();
 
 private:
 
 };
 
 template <class Type>
-Tyoe Queue<Type> :: remove(int index)
+Queue<Type> :: Queue() : DoublyLinkedList<Type>()
+{
+
+}
+
+/*
+ destructor as list, array, stack... since it is a linear data structure.
+ */
+template<class Type>
+Queue<Type> :: ~Queue()
+{
+	BiDirectionalNode<Type> * remove = this->getFront();
+
+	while(this->getFront() != nullptr)
+	{
+		this->setFront(this->getFront()->getNextPointer());
+		delete remove;
+		remove = this->getFront();
+	}
+}
+
+template <class Type>
+void Queue<Type> :: add(Type value)
+{
+	enqueue(value);
+}
+
+template <class Type>
+Type Queue<Type> :: remove(int index)
 {
 	assert(index == 0 && this->getSize() > 0);
 	return dequeue();
+}
+
+/*
+ 1. create a node pointer
+ 2. if size == 0, adjust front to point to the new node
+ 3. else, set new node to ends next, connect new nodes to previous end
+ 4. move end to the new node
+ 5. adjust size + 1
+ */
+template <class Type>
+void Queue<Type> :: enqueue(Type data)
+{
+	BiDirectionalNode<Type> * added = new BiDirectionalNode<Type>(insertedValue);
+
+	if(this->getSize == 0 || this->getFront() == nullptr || this-getEnd() == nullptr)
+	{
+		this->setFront(added);
+	}
+	else
+	{
+		this->getEnd()->setNextPointer(added);
+		added->setPreviousPointer(this->getEnd());
+	}
+
+	this->setEnd(added);
+	this->setSize(this->getSize() + 1);
 }
 
 /*
