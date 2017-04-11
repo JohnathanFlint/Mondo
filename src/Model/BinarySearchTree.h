9559c964778b0e60f2872a5fc0962846a1de4758
JohnathanFlint <nicholas.white142@gmail.com>
2017-04-11 09:24:29 -0600

    updated umm for BinarySearchTree and created the header for BinarySearchTree

---
+++ src/Model/BinarySearchTree.h
@@ -0,0 +1,24 @@
+/*
+ * BinarySearchTree.h
+ *
+ *  Created on: Apr 11, 2017
+ *      Author: nwhi5696
+ */
+
+#ifndef MODEL_BINARYSEARCHTREE_H_
+#define MODEL_BINARYSEARCHTREE_H_
+
+#include "Tree.h"
+
+template <class Type>
+class BinarySearchTree : public Tree<Type>
+{
+private:
+
+public:
+
+};
+
+
+
+#endif /* MODEL_BINARYSEARCHTREE_H_ */
